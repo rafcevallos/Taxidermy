44f561e6084d14144a76b2df07cb46124125b2f9
rafcevallos <rafael.cevallos81@gmail.com>
2018-05-08 15:55:34 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+const listAnimals = require("./dom/DisplayCatalog")
+
+// Display a list of all animals
+listAnimals()
+
+
+// The only modules needed on main.js are ones that will display what the USER WILL SEE FIRST. Other modules need to be broken up and put in the appropriate js files
+
+// APIManager.getAllAnimals().then(allAnimals => console.log(allAnimals))
